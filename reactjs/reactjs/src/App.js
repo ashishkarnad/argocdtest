657912e1da8d9cf6206515ea64fbdf4fe237b42f
ashish <ashish@hplocal>
2025-02-13 21:38:05 +0530

    reactapp

---
+++ reactjs/reactjs/src/App.js
@@ -0,0 +1,36 @@
+// src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, Dockerized React!</h1>
+      <p>This is a simple React application running in Docker.</p>
+    </div>
+  );
+}
+
+export default App;
+//########################
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
